9cfd55724726a4ce422db2adbcdec3aabcbaceb1
yCAFEEE <yuridan2705@gmail.com>
2026-03-01 18:19:16 -0300

    remove default react css and add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home.jsx"
 
 function App() {
-  const [city, setCity] = useState('');
-  const [weather, setWeather] = useState(null);
-
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-
-    try{
-      const response = await fetch("http://127.0.0.1:8000/", {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({ city }),
-      });
-      const data = await response.json();
-
-      if(response.ok){
-        setWeather(data.weather);
-      }else{
-        setWeather(null);
-      }
-    } catch(err){
-      console.log("ESSE AQUI É O ERRO (SE TIVER): ", err);
-    }
-  };
-  
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type='text' 
-          placeholder='City name...'
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button type='submit'>Get weather</button>
-      </form>
-      
-      {weather && (
-        <div className='weather-container'>
-          <h1>{weather.city}</h1>
-          <p>Temperature: {weather.temperature} °C</p>
-          <p>Humidity: {weather.humidity}%</p>
-          <p>{weather.description}</p>
-          <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Weather icon" />
-        </div>
-      )}
-    </>
-  )
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './Home.css'
+
+export default function Home(){
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    try{
+      const response = await fetch("http://127.0.0.1:8000/", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({ city }),
+      });
+      const data = await response.json();
+
+      if(response.ok){
+        setWeather(data.weather);
+      }else{
+        setWeather(null);
+      }
+    } catch(err){
+      console.log("ESSE AQUI É O ERRO (SE TIVER): ", err);
+    }
+  };
+  
+  return (
+    <>
+      <form onSubmit={handleSubmit} id='search-form'>
+        <input
+          id='city-name'
+          type='text' 
+          placeholder='City name...'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type='submit'>Get weather</button>
+      </form>
+      
+      {weather && (
+        <div className='weather-container'>
+          <h1>{weather.city}</h1>
+          <div className='temp-container'>
+            <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Weather icon" />
+            <h1>{weather.temperature} °C</h1>
+          </div>
+          <p>{weather.description}</p>
+          <p>Humidity: {weather.humidity}%</p>
+        </div>
+      )}
+    </>
+  )
+}
